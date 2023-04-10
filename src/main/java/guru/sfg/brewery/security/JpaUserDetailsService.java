c50ccd8d1ee6e702579d853a84c38e4470cdcf33
hashemmohamed98 <hashem.mhashem98@gmail.com>
2023-04-10 04:56:48 +0200

    Committed

--- src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
+++ src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
@@ -33,7 +33,7 @@ public class JpaUserDetailsService implements UserDetailsService {
 private final  UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    log.debug("Loading User Credintials Via JPA");
+    log.debug("Loading User Credintials Via JPA ");
        return userRepository.findByUsername(username).orElseThrow(() ->{
         return new  UsernameNotFoundException("User name: "+username +" does not exist");
         });
